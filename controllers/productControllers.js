29e4580d35fa6acf78d7b4155fa6a1e94cb215b9
devvyky <okanemechibuikem@gmail.com>
2021-02-15 19:31:53 +0100

    feat: refactor api features

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.createProduct = catchAsync(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -18,50 +19,13 @@ exports.createProduct = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
-  // Build the query
-  // 1A) Filtering
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // 1B) Advance Filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  let query = Product.find(JSON.parse(queryStr));
-
-  // 2) Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  // 3) Fields Limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-  } else {
-    query = query.select('-__v');
-  }
-
-  // 4) Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 100;
-  const skip = (page - 1) * limit;
-
-  // page=3&limit=10, 1- 10, page 1, 11-20, page 2, 21-30 page 3
-  query = query.skip(skip).limit(limit);
-
-  if (req.query.page) {
-    const numProducts = await Product.countDocuments();
-    if (skip >= numProducts)
-      throw new AppError('This page does not exist! 😰', 404);
-  }
-
   // Execute the query
-  const products = await query;
+  const features = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const products = await features.query;
 
   if (!products) {
     return next(new AppError('No Product Found! 😰', 404));
---
+++ utils/apiFeatures.js
@@ -0,0 +1,61 @@
+class APIFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryStr };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryStr.page * 1 || 1;
+    const limit = this.queryStr.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=3&limit=10, 1- 10, page 1, 11-20, page 2, 21-30 page 3
+    this.query = this.query.skip(skip).limit(limit);
+
+    // if (this.queryStr.page) {
+    //   const numProducts = await Product.countDocuments();
+    //   if (skip >= numProducts)
+    //     throw new AppError('This page does not exist! 😰', 404);
+    // }
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
